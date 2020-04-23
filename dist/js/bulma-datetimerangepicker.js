8104c90fe4038c758512ec7bb8cf272e88c681ab
Daniel Miedzik <daniel.miedzik@gmail.com>
2020-04-23 12:29:46 +0200

    use new package name

--- dist/js/bulma-datetimerangepicker.js
+++ dist/js/bulma-datetimerangepicker.js
@@ -4,9 +4,9 @@
 	else if(typeof define === 'function' && define.amd)
 		define([], factory);
 	else if(typeof exports === 'object')
-		exports["bulmaCalendar"] = factory();
+		exports["bulmaDatetimerangepicker"] = factory();
 	else
-		root["bulmaCalendar"] = factory();
+		root["bulmaDatetimerangepicker"] = factory();
 })(typeof self !== 'undefined' ? self : this, function() {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
